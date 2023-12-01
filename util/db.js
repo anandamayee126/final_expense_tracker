ac17a99e073be3f656a480ea64c58b2bd3479a1c
Anandamayee Ghosh <ghosh.anandamayee>
2023-12-01 10:23:13 +0530

    TS started

--- util/db.js
+++ util/db.js
@@ -1,7 +1,6 @@
+const Sequelize= require('sequelize');
 const dot_env= require('dotenv');
 dot_env.config();
-
-const Sequelize= require('sequelize');
 const sequelize= new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{dialect:'mysql', host: 'localhost'});
 
 module.exports= sequelize;
