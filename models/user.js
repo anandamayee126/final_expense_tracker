2e217a59b4a643a6eb96e48bdbca19960582c1d4
Anandamayee Ghosh <ghosh.anandamayee>
2024-02-01 11:53:21 +0530

    databse connection done

--- models/user.js
+++ models/user.js
@@ -1,32 +1,44 @@
-// const Sequelize = require('sequelize');
-// const sequelize = require('../util/db');
-// const User= sequelize.define('user',{
-//     id:{
-//         type: Sequelize.INTEGER,
-//         autoIncrement: true,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     name:{
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     email:{
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     password:{
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     isPremiumUser:{
-//         type: Sequelize.BOOLEAN,
-//         defaultValue: false
-//     },
-//     totalExpense:{
-//         type: Sequelize.INTEGER,
-//         defaultValue:0
-//     }
-// })
+const getDb= require('../util/db').getDb;
+class User{
+    constructor(id,name,email,password,isPremiumUser,totalExpense){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.isPremiumUser=isPremiumUser;
+        this.totalExpense=totalExpense;
+    }
+    save(){
+        
+    }
+}
+const User= sequelize.define('user',{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    isPremiumUser:{
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    },
+    totalExpense:{
+        type: Sequelize.INTEGER,
+        defaultValue:0
+    }
+})
 
-// module.exports= User;
+module.exports= User;
--- util/db.js
+++ util/db.js
@@ -1,13 +1,23 @@
 const mongo= require('mongodb');
 const mongoClient= mongo.MongoClient;
+let _db;
 
 const mongoConnect= (callback)=>{
         mongoClient.connect('mongodb+srv://ghoshanandamayee:andy@12AB@nosqlcluster.aykufoh.mongodb.net/?retryWrites=true&w=majority').then((client) => {
-        callback(client);
-        console.log("Connected!!");
+            _db=client.db();
+            callback();
+            console.log("Connected!!");
     }).catch((err) => {
         console.log(err);
     })
 }
 
-module.exports=mongoConnect;
+const getDb= function(){
+    if(_db){
+        return _db;
+    }
+    throw "No database found";
+}
+
+exports.mongoConnect= mongoConnect;
+exports.getDb=getDb;
